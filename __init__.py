810694c651bc28797ebc953c76e4034bc2b22bf7
kuojiusheng <love1357983@gmail.com>
2017-05-21 17:28:07 +0800

    update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,9 +4,14 @@ import os
 import re
 
 from subprocess import check_output, CalledProcessError
+from flask import Flask, Response, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-from flask import Flask, Response
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['apk'])
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def split_lines(s):
     """Splits lines in a way that works even on Windows and old devices.
@@ -69,65 +74,47 @@ def hello():
             continue
         else:
             devices.append("<td>")
-            info = line.split('\t')
-            devices.append(info[0])
+            devices.append(line)
             devices.append("</td>")
-
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'getprop ro.product.model'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append(adb_output)
-            devices.append("</td>")
-	
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'getprop ro.product.cpu.abi'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append(adb_output)
-            devices.append("</td>")
-	
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'getprop ro.sf.lcd_density'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append(adb_output)
-            devices.append("</td>")
-
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'wm size'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append(adb_output)
-            devices.append("</td>")
-	
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'getprop ro.build.version.release'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append("Android ")
-            devices.append(adb_output)
-            devices.append("</td>")
-	
-            devices.append("<td>")
-            adb_cmd = ['adb']
-            adb_cmd.extend(['-s' , info[0]])
-            adb_cmd.extend(['shell' , 'getprop ro.build.version.sdk'])
-            adb_output = subprocess.check_output(adb_cmd)
-            devices.append("API ")
-            devices.append(adb_output)
-            devices.append("</td>")
-	
+        
         devices.append("</tr>")
 	adb_cmd = ['adb']
     devices.append("<table>")
     ret = ''.join(devices) 
     return Response(ret)
 
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            # after uploads file to change url
+            return redirect(url_for('foo'))
+    return '''
+        <!doctype html>
+        <title>Upload new File</title>
+        <h1>Upload new File</h1>
+        <form method=post enctype=multipart/form-data>
+        <p><input type=file name=file>
+        <input type=submit value=Upload>
+        </form>
+        '''
+
+@app.route('/foo')
+def foo():
+    return 'Hello Foo!'
+
 if __name__ == "__main__":
+    app.debug = True
     app.run(host="127.0.0.1")
