74df16c286ed2fd2ea3298c4e72f36e32e07e3f0
KuoJiuSheng <love1357983@gmail.com>
2017-07-26 16:03:15 +0800

    add testing project result to more devices

--- __init__.py
+++ __init__.py
@@ -4,10 +4,12 @@ import os
 import re
 import math
 import string
+import threading
 
 from subprocess import check_output, CalledProcessError
 from flask import Flask, Response, request, redirect, url_for
 from werkzeug.utils import secure_filename
+from time import localtime, strftime
 
 from ftplib import FTP
 
@@ -21,9 +23,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def split_lines(s):
     """Splits lines in a way that works even on Windows and old devices.
-    Windows will see \r\n instead of \n, old devices do the same, old devices
-    on Windows will see \r\r\n.
-    """
+        Windows will see \r\n instead of \n, old devices do the same, old devices
+        on Windows will see \r\r\n.
+        """
     # rstrip is used here to workaround a difference between splineslines and
     # re.split:
     # >>> 'foo\n'.splitlines()
@@ -40,30 +42,42 @@ def home():
     devices.append("<table>")
     devices.append("<tr>")
     
+    # Devices Serialno
     devices.append("<td>")
     devices.append("serialno")
     devices.append("</td>")
     
+    # Devices Model Name
     devices.append("<td>")
     devices.append("model name")
     devices.append("</td>")
     
+    # Devices CPU
     devices.append("<td>")
     devices.append("cpu")
     devices.append("</td>")
     
+    # Devices Density
     devices.append("<td>")
     devices.append("density")
     devices.append("</td>")
     
+    # Devices Size
     devices.append("<td>")
     devices.append("size")
     devices.append("</td>")
     
+    # Devices Board Specifications
+    devices.append("<td>")
+    devices.append("Board Specifications")
+    devices.append("</td>")
+    
+    # Devices release
     devices.append("<td>")
     devices.append("release")
     devices.append("</td>")
     
+    # Devices API Level
     devices.append("<td>")
     devices.append("API Level")
     devices.append("</td>")
@@ -79,11 +93,13 @@ def home():
         if '* daemon not running. starting it now at tcp:5037 *' in line or 'daemon started successfully' in line:
             continue
         else:
+            # Devices Serialno
             devices.append("<td>")
             info = line.split('\t')
             devices.append(info[0])
             devices.append("</td>")
             
+            # Devices Model Name
             devices.append("<td>")
             cmd_adb_get_devices_model = ['adb']
             cmd_adb_get_devices_model.extend(['-s' , info[0]])
@@ -92,6 +108,7 @@ def home():
             devices.append(cmd_adb_get_devices_model)
             devices.append("</td>")
             
+            # Devices CPU
             devices.append("<td>")
             cmd_adb_get_devices_cpu = ['adb']
             cmd_adb_get_devices_cpu.extend(['-s' , info[0]])
@@ -100,6 +117,7 @@ def home():
             devices.append(cmd_adb_get_devices_cpu)
             devices.append("</td>")
             
+            # Devices Density
             devices.append("<td>")
             cmd_adb_get_devices_lcd_density = ['adb']
             cmd_adb_get_devices_lcd_density.extend(['-s' , info[0]])
@@ -108,6 +126,7 @@ def home():
             devices.append(cmd_adb_get_devices_lcd_density)
             devices.append("</td>")
             
+            # Devices Size
             devices.append("<td>")
             cmd_adb_get_devices_size = ['adb']
             cmd_adb_get_devices_size.extend(['-s' , info[0]])
@@ -117,6 +136,7 @@ def home():
             devices.append(devices_split[1])
             devices.append("</td>")
             
+            # Devices Board Specifications
             devices.append("<td>")
             devices_size = devices_split[1].split('x')
             display_size = math.sqrt(pow(float(devices_size[0])/float(cmd_adb_get_devices_lcd_density),2)+pow(float(devices_size[1])/float(cmd_adb_get_devices_lcd_density),2))
@@ -126,6 +146,7 @@ def home():
                 devices.append('SmartPhone')
             devices.append("</td>")
             
+            # Devices release
             devices.append("<td>")
             cmd_adb_get_devices_version_release = ['adb']
             cmd_adb_get_devices_version_release.extend(['-s' , info[0]])
@@ -135,6 +156,7 @@ def home():
             devices.append(cmd_adb_get_devices_version_release)
             devices.append("</td>")
             
+            # Devices API Level
             devices.append("<td>")
             cmd_adb_get_devices_api_level = ['adb']
             cmd_adb_get_devices_api_level.extend(['-s' , info[0]])
@@ -145,15 +167,15 @@ def home():
             devices.append("</td>")
         
         devices.append("</tr>")
-
+    
     devices.append("<table>")
-    ret = ''.join(devices) 
+    ret = ''.join(devices)
     return Response(ret)
 
 
 def allowed_file(filename):
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/uploads', methods=['GET', 'POST'])
 def upload_file():
@@ -172,10 +194,10 @@ def upload_file():
         else:
             apk_file_name_array = apk_file.split("/")
             apk_file_name = apk_file_name_array[len(apk_file_name_array)-1]
-        
+            
             apk_test_file_array = apk_test_file.split("/")
             apk_test_file_name = apk_test_file_array[len(apk_test_file_array)-1]
-
+            
             ftp.storbinary("STOR /Users/kuo/Documents/GitHub/yzu/scaleout-test-prog/uploads/" + apk_file_name, open(apk_file, 'rb'))
             ftp.storbinary("STOR /Users/kuo/Documents/GitHub/yzu/scaleout-test-prog/uploads/" + apk_test_file_name, open(apk_test_file, 'rb'))
             ftp.quit()
@@ -195,17 +217,83 @@ def get_apk_package_name(test_project_name):
         cmd_aapt_output = subprocess.check_output(cmd_get_apk_package_name)
         apk_file_name.append(cmd_aapt_output)
         apk_file_name.append('<br>')
-
+    
     ret = ''.join(apk_file_name)
+
+class threadServer(threading.Thread):
+    def __init__(self, test_project_name, nowTime, device_name):
+        threading.Thread.__init__(self)
+        self.pro_name = test_project_name
+        self.Time = nowTime
+        self.dev_name = device_name
+        self.lock = threading.Lock()
+    
+    def run(self):
+        self.lock.acquire()
+        cmd_get_apk_package_name = ['./testing_project.sh', self.pro_name, self.Time, self.dev_name]
+        cmd_testing_output = subprocess.check_output(cmd_get_apk_package_name)
+        self.lock.release()
 
 @app.route('/testing_project', methods=['GET', 'POST'])
 def testing_project():
     if request.method == 'POST':
+        threads = []
+        devices = []
+        #catch serial number
+        out = split_lines(subprocess.check_output(['adb', 'devices']))
+        for line in out[1:]:
+            if '* daemon not running. starting it now at tcp:5037 *' in line or 'daemon started successfully' in line:
+                continue
+            else:
+                info = line.split('\t')
+                devices.append(info[0])
+        
+        #catch project name
+        print "Getting test project name."
         test_project_name = request.form.get('test_project_name')
-        cmd_get_apk_package_name = ['./testing_project.sh', test_project_name]
-        cmd_testing_output = subprocess.check_output(cmd_get_apk_package_name)
-        return cmd_testing_output
-
+        if not test_project_name == 'null':
+            print "Test project name: {0}".format(test_project_name)
+        else:
+            print "Can't get test project name."
+            return "Error. Can't get test project name."
+        #catch device amount
+        print "Getting test device amount."
+        test_device_amount = request.form.get('test_device_amount')
+        if not test_project_name == 'null':
+            print "Test device amount: {0}".format(test_device_amount)
+        else:
+            print "Can't get test device amount."
+            return "Error. Can't get test device amount."
+        
+        count = 0
+        isCompleteAll = False
+        device_amount = int(test_device_amount, 10)
+        
+        if device_amount == 0:
+            return "Error: test_device_amout = 0"
+        
+        #get current time
+        print "Getting time."
+        nowTime = strftime('%Y-%m-%d_%H_%M_%S', localtime())
+        print "Current time: " + nowTime
+        
+        #processins multi-threading
+        for i in xrange(device_amount):
+            print "{0} processing...".format(devices[count])
+            #to create and start the thread then append it to threads
+            t = threadServer(test_project_name, nowTime, devices[count])
+            t.start()
+            threads.append(t)
+            count += 1
+            if count == device_amount:
+                isCompleteAll = True
+                break
+    
+        if isCompleteAll:
+            return "All projects complete."
+        else:
+            return "{0} tested. {1} left.".format(count, device_amount)
+    
     return '''
         Please re-enter the command
         '''
--- testing_project.sh
+++ testing_project.sh
@@ -1,11 +1,32 @@
 #!/bin/bash
 
-
 test_project_name=$1
+
+test_data=$2
+
+test_device_serial_number=$3
 
 # Add aapt to path
 for aapt_path in ${ANDROID_HOME}/build-tools/*/; do break; done
 export PATH="$PATH:${aapt_path}"
+
+if [ ! -d "testing_result/$test_project_name" ]; then
+
+    mkdir testing_result/$test_project_name
+
+fi
+
+if [ ! -d "testing_result/$test_project_name/$test_data" ]; then
+
+    mkdir testing_result/$test_project_name/$test_data
+
+fi
+
+if [ ! -d "testing_result/$test_project_name/$test_data/$test_device_serial_number" ]; then
+
+    mkdir testing_result/$test_project_name/$test_data/$test_device_serial_number
+
+fi
 
 if [ -d "uploads/$test_project_name" ]; then
 
@@ -20,17 +41,22 @@ if [ -d "uploads/$test_project_name" ]; then
     apk_test_package=`aapt dump badging uploads/$test_project_name/apk_test_file/$apk_test_file | grep package | awk '{print $2}' | sed s/name=//g | sed s/\'//g`
 
     # 因為在此無法立即回應，所以我們先將我們的結果放置在設定好的文件當中
-    echo `adb push uploads/$test_project_name/apk_file/$app_file data/local/tmp/$apk_package` >> uploads/$test_project_name/apk_push
+    echo `adb -s $test_device_serial_number push uploads/$test_project_name/apk_file/$app_file data/local/tmp/$apk_package` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/apk_push
 
-    echo `adb shell pm install -r "/data/local/tmp/$apk_package"` >> uploads/$test_project_name/apk_install
+    echo `adb -s $test_device_serial_number shell pm install -r "/data/local/tmp/$apk_package"` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/apk_install
 
-    echo `adb push uploads/$test_project_name/apk_test_file/$apk_test_file data/local/tmp/$apk_test_package` >> uploads/$test_project_name/test_apk_push
+    echo `adb -s $test_device_serial_number push uploads/$test_project_name/apk_test_file/$apk_test_file data/local/tmp/$apk_test_package` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/test_apk_push
 
-    echo `adb shell pm install -r "/data/local/tmp/$apk_test_package"` >> uploads/$test_project_name/test_apk_install
+    echo `adb -s $test_device_serial_number shell pm install -r "/data/local/tmp/$apk_test_package"` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/test_apk_install
 
-    echo `adb shell am instrument -w $apk_test_package/android.support.test.runner.AndroidJUnitRunner` >> uploads/$test_project_name/report
+    echo `adb -s $test_device_serial_number shell am instrument -w $apk_test_package/android.support.test.runner.AndroidJUnitRunner` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/report
 
-    echo `cat uploads/$test_project_name/report`
+    echo `adb -s $test_device_serial_number shell pm uninstall $apk_package` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/apk_uninstall
+
+    echo `adb -s $test_device_serial_number shell pm uninstall $apk_test_package` >> testing_result/$test_project_name/$test_data/$test_device_serial_number/test_apk_uninstall
+
+
+    echo `cat testing_result/$test_project_name/$test_data/$test_device_serial_number/report`
 
 else
 
